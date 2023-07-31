684d4830ce2178c7808af367541ae176a47be0e5
Jayveersinh Raj <69463767+Jayveersinh-Raj@users.noreply.github.com>
2023-07-31 14:46:09 +0300

    custom utils python file for reusability

--- helper_utils.py
+++ helper_utils.py
@@ -2,6 +2,81 @@ import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import torch
+import time
+
+def binary_infer(df: pd.DataFrame, column: str, model, tokenizer) -> None:
+  """
+  A function to take inference for binary classification on a sentence.
+
+  Parameters:
+  ------------
+  df : The dataframe (test maybe)
+  colum : The target column that contains the sentences
+  model : The model to get inference on
+  tokenizer: The tokenizer to tokenize sentences
+
+  Returns:
+  -----------
+  None
+  """
+  
+  # Define the device (GPU or CPU)
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  
+  # Move the model to the appropriate device
+  model = model.to(device)
+  
+  # Initialize an empty list to store the predictions
+  predictions = []
+  
+  # Define the batch size and total time variables
+  batch_size = 100
+  total_time = 0.0
+  
+  # Split the dataframe into batches
+  for i in range(0, len(df), batch_size):
+      batch_texts = df[column].iloc[i:i+batch_size].tolist()
+  
+      # Tokenize the batch of texts and move to the device
+      inputs = tokenizer.batch_encode_plus(batch_texts, return_tensors="pt", max_length=512, truncation=True, padding=True)
+      inputs = {key: val.to(device) for key, val in inputs.items()}
+  
+      # Perform the model inference on the batch
+      with torch.no_grad():  # Disable gradient calculation during inference
+          start_time = time.time()  # Start time for the batch
+          outputs = model(**inputs)
+          probabilities = torch.softmax(outputs.logits, dim=1)
+          predicted_classes = torch.argmax(probabilities, dim=1)
+          end_time = time.time()  # End time for the batch
+  
+      # Calculate the time taken for the batch
+      batch_time = end_time - start_time
+  
+      # Map predicted class indices to labels
+      batch_pred_labels = [1 if pred == 1 else 0 for pred in predicted_classes.tolist()]
+  
+      # Extend the predictions list with batch results
+      predictions.extend(batch_pred_labels)
+  
+      # Accumulate batch time to the total time
+      total_time += batch_time
+  
+      # Clear variables and tensors that are not required
+      del inputs, outputs, probabilities, predicted_classes, batch_texts, batch_pred_labels
+      torch.cuda.empty_cache()  # Clear GPU cache if applicable
+  
+      # Print time taken for the current batch
+      print(f"Batch {i//batch_size+1}: {batch_time:.4f} seconds")
+  
+  # Assign the predictions to the dataframe
+  df['pred_lab'] = predictions
+  
+  # Print total time taken for processing the entire data
+  print(f"Total time taken for the entire data: {total_time:.4f} seconds")
+
+
+
 
 # Our function needs a different name to sklearn's plot_confusion_matrix
 def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
@@ -74,7 +149,9 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   # Save the figure to the current working directory
   if savefig:
     fig.savefig("confusion_matrix.png")
-  
+
+
+
 def top_1_accuracy(y_true, y_pred):
     """
     Computes the top-1 accuracy of a classifier.
@@ -100,6 +177,8 @@ def top_1_accuracy(y_true, y_pred):
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score, roc_curve
+
+
 
 def calculate_and_plot_auc_roc(ground_truth, predicted_probs):
     """
